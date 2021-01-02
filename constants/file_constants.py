8d8953a603f986743ec88aa2618d11675d99e228
David Wintermeyer <dwinterm@amazon.com>
2021-01-02 11:24:23 -0800

    Local file path

--- constants/file_constants.py
+++ constants/file_constants.py
@@ -1,2 +1,4 @@
-SIT_REP_FILE_PATH = '/Users/dwinterm/PycharmProjects/DadSitRepForMason/Covid-19_SitRep_Data.xlsx'
+# Local filepath
+# https://openpyxl.readthedocs.io/en/stable/tutorial.html#loading-from-a-file
+SIT_REP_FILE_PATH = 'Covid-19_SitRep_Data.xlsx'
 SHEET_NAME = 'Sheet1'
