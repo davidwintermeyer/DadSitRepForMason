1507579eb64ce974d0a8859fe61f623c734e8ce6
David Wintermeyer <dwinterm@amazon.com>
2021-01-10 10:13:41 -0800

    Revert "excel name_styles avoid collision"
    This reverts commit 0719aa4532eebf0643c527ba132b7d330fed06f4.

--- constants/file_constants.py
+++ constants/file_constants.py
@@ -25,38 +25,32 @@ SHEET_NAME = 'Sheet1'
 # https://stackoverflow.com/questions/24370385/how-to-format-cell-with-datetime-object-of-the-form-yyyy-mm-dd-hhmmss-in-exc
 # https://support.microsoft.com/en-us/office/format-a-date-the-way-you-want-8e10019e-d5d8-47a1-ba95-db95123d273e
 # the names must be unique
+DATE_STYLE = NamedStyle(name='date_style', number_format='d-mmm')
+TIME_STYLE = NamedStyle(name='time_style', number_format='HH:MM')
+NUMBER_STYLE = NamedStyle(name='number_style', number_format='#,###')
+PERCENT_STYLE = NamedStyle(name='percent_style', number_format='#.#%')
 
-
-def get_style_dict(report_date: date) -> dict:
-    date_string = report_date.strftime('%m-%d-%Y')
-    date_style = NamedStyle(name='date_style' + date_string, number_format='d-mmm')
-    time_style = NamedStyle(name='time_style' + date_string, number_format='HH:MM')
-    number_style = NamedStyle(name='number_style' + date_string, number_format='#,###')
-    percent_style = NamedStyle(name='percent_style'+ date_string, number_format='#.#%')
-
-    style_dict = {
-        sitrep_column_constants.DATE_COLUMN: date_style,
-        sitrep_column_constants.TIME_COLUMN: time_style,
-        sitrep_column_constants.GLOBAL_CASES_COLUMN: number_style,
-        sitrep_column_constants.GLOBAL_DEATHS_COLUMN: number_style,
-        sitrep_column_constants.US_CASES_COLUMN: number_style,
-        sitrep_column_constants.US_DEATHS_COLUMN: number_style,
-        sitrep_column_constants.VA_CASES_TOTAL_COLUMN: number_style,
-        sitrep_column_constants.VA_POSITIVE_TEST_RATE_COLUMN: percent_style,
-        sitrep_column_constants.VA_DEATHS_TOTAL_COLUMN: number_style,
-        sitrep_column_constants.VA_PRESENT_HOSPITALIZATIONS_CURRENT_COLUMN: number_style,
-        sitrep_column_constants.FAIRFAX_CASES_COLUMN: number_style,
-        sitrep_column_constants.FAIRFAX_POSITIVE_TEST_RATE_COLUMN: percent_style,
-        sitrep_column_constants.FAIRFAX_HOSPITALIZATIONS_COLUMN: number_style,
-        sitrep_column_constants.FAIRFAX_DEATHS_COLUMN: number_style,
-        sitrep_column_constants.ARLINGTON_CASES_COLUMN: number_style,
-        sitrep_column_constants.ARLINGTON_POSITIVE_TEST_RATE_COLUMN: percent_style,
-        sitrep_column_constants.ARLINGTON_HOSPITALIZATIONS_COLUMN: number_style,
-        sitrep_column_constants.ARLINGTON_DEATHS_COLUMN: number_style,
-        sitrep_column_constants.PW_CASES_COLUMN: number_style,
-        sitrep_column_constants.PW_POSITIVE_TEST_RATE_COLUMN: percent_style,
-        sitrep_column_constants.PW_HOSPITALIZATIONS_COLUMN: number_style,
-        sitrep_column_constants.PW_DEATHS_COLUMN: number_style
-    }
-
-    return style_dict
+STYLE_DICT = {
+    sitrep_column_constants.DATE_COLUMN: DATE_STYLE,
+    sitrep_column_constants.TIME_COLUMN: TIME_STYLE,
+    sitrep_column_constants.GLOBAL_CASES_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.GLOBAL_DEATHS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.US_CASES_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.US_DEATHS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.VA_CASES_TOTAL_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.VA_POSITIVE_TEST_RATE_COLUMN: PERCENT_STYLE,
+    sitrep_column_constants.VA_DEATHS_TOTAL_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.VA_PRESENT_HOSPITALIZATIONS_CURRENT_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.FAIRFAX_CASES_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.FAIRFAX_POSITIVE_TEST_RATE_COLUMN: PERCENT_STYLE,
+    sitrep_column_constants.FAIRFAX_HOSPITALIZATIONS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.FAIRFAX_DEATHS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.ARLINGTON_CASES_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.ARLINGTON_POSITIVE_TEST_RATE_COLUMN: PERCENT_STYLE,
+    sitrep_column_constants.ARLINGTON_HOSPITALIZATIONS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.ARLINGTON_DEATHS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.PW_CASES_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.PW_POSITIVE_TEST_RATE_COLUMN: PERCENT_STYLE,
+    sitrep_column_constants.PW_HOSPITALIZATIONS_COLUMN: NUMBER_STYLE,
+    sitrep_column_constants.PW_DEATHS_COLUMN: NUMBER_STYLE
+}
