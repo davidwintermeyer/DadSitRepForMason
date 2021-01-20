90d96772e6c6e9c4beef51839e693003593ff07b
David Wintermeyer <dwinterm@amazon.com>
2021-01-20 12:40:40 -0800

    adding columns for change since previous day

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from pytz import timezone
 
 from constants import file_constants
-from generate_sit_rep import generate_report_for_day
+from generate_sit_rep import generate_report_for_day_write_file_locally
 
 # Set Current date and time
 from util.date_util import get_days_ago
 
 tz = timezone('EST')
 report_date = datetime.now(tz).date()
-# report_date = get_days_ago(report_date, 1)
+report_date = get_days_ago(report_date, 7)
 report_time = datetime.now(tz).time()
 
 # Currently, reads file from file_constants.SIT_REP_FILE_PATH and then overwrites it
@@ -19,4 +19,4 @@ input_file_path = file_constants.SIT_REP_FILE_PATH
 output_file_path = file_constants.SIT_REP_FILE_PATH
 
 # Executes the generate_report_for_day
-generate_report_for_day(input_file_path, output_file_path, report_date, report_time)
+generate_report_for_day_write_file_locally(input_file_path, output_file_path, report_date, report_time)
