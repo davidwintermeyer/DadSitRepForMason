d998cbfd6016f554dc5b10160c3e0e567ec8c4fa
David Wintermeyer <dwinterm@amazon.com>
2021-03-15 21:31:03 -0700

    uploading works

--- uploading_files/s3_uploadSitRep.js
+++ uploading_files/s3_uploadSitRep.js
@@ -45,35 +45,16 @@ var s3 = new AWS.S3({
 });
 // snippet-end:[s3.JavaScript.photoAlbumExample.config]
 
+// I'm leaving this with the S3 objects even though I don't care about it at all.
 function uploadSitRepEntryPoint() {
   s3.listObjects({ Delimiter: "/" }, function(err, data) {
     if (err) {
       return alert("There was an error listing your albums: " + err.message);
     } else {
-      var albums = data.CommonPrefixes.map(function(commonPrefix) {
-        var prefix = commonPrefix.Prefix;
-        var albumName = decodeURIComponent(prefix.replace("/", ""));
-        return getHtml([
-          "<li>",
-          "<span onclick=\"deleteAlbum('" + albumName + "')\">X</span>",
-          "<span onclick=\"viewAlbum('" + albumName + "')\">",
-          albumName,
-          "</span>",
-          "</li>"
-        ]);
-      });
-      var message = albums.length
-        ? getHtml([
-            "<p>Click on an album name to view it.</p>",
-            "<p>Click on the X to delete the album.</p>"
-          ])
-        : "<p>You do not have any albums. Please Create album.";
       var htmlTemplate = [
-        "<h2>Albums</h2>",
-        message,
-        "<ul>",
-        getHtml(albums),
-        "</ul>",
+        '<p>Select the sitrep to upload using the *Choose File* button, then click uploadSitRep',
+        '<br>',
+        '<br>',
         '<input id="sitrepupload" type="file" accept="application/*">',
         "<button onclick=\"uploadSitRep()\">",
         "Upload Sit Rep",
